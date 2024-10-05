430913f330dc2c167332474dc4553be3b294864c
Glitch (eat-radium) <none>
2024-10-05 02:49:49 +0000

    💕🍄 Checkpoint ./server/modules/definitions/gunvals.js:66976691/65 ./server/modules/definitions/groups/tanks.js:66976691/2043

--- server/modules/definitions/groups/tanks.js
+++ server/modules/definitions/groups/tanks.js
@@ -5077,7 +5077,67 @@ Class.turbinate = {
 }
     ]
 }
-
+Class.riposte = {
+    PARENT: "genericTank",
+    LABEL: "Riposte",
+    BODY: {
+        HEALTH: base.HEALTH * 0.4,
+        SHIELD: base.SHIELD * 0.4,
+        DENSITY: base.DENSITY * 0.3
+    },
+    DANGER: 7,
+    GUNS: [
+        {
+            POSITION: [18, 8, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.triAngleFront, { recoil: 4 }]),
+                TYPE: "bullet",
+                LABEL: "Front"
+            }
+        },
+        {
+            POSITION: [14, 8, 1, 0, -1, 140, 0.6],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
+                TYPE: "bullet",
+                LABEL: "thruster"
+            }
+        },
+        {
+            POSITION: [14, 8, 1, 0, 1, -140, 0.6],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
+                TYPE: "bullet",
+                LABEL: "thruster"
+            }
+        },
+        {
+            POSITION: [16, 8, 1, 0, 0, 150, 0.1],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
+                TYPE: "bullet",
+                LABEL: "thruster"
+            }
+        },
+        {
+            POSITION: [16, 8, 1, 0, 0, -150, 0.1],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.flankGuard, g.triAngle, g.thruster]),
+                TYPE: "bullet",
+                LABEL: "thruster"
+            }
+        },
+        {
+            POSITION: [0, 0, 1, 0, 0, -180, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.riposte]),
+                TYPE: "bullet",
+                LABEL: "riposte",
+                ALT_FIRE: true,
+            }
+        }
+    ]
+}
 
 
 // Upgrade Paths
