800b2f0bc6afe6e44d64676604240e55db69838b
Glitch (eat-radium) <none>
2024-10-10 13:13:36 +0000

    ⛄️😄 Checkpoint ./server/config.js:66976691/20

--- server/config.js
+++ server/config.js
@@ -26,7 +26,7 @@ module.exports = {
 
     // The \modules\setup\gamemodeconfigs\ files to load.
     // To change specific things about specific gamemodes (such as team count for tdm), edit their config file in \modules\setup\gamemodeconfigs\.
-    GAME_MODES: ['growth', 'maze', 'ffa'],
+    GAME_MODES: ['ffa'],
 
     // The room files to load in the setup/rooms folder.
     // NOTE: If a /gamemodeconfig/ file "replaces" the value of ROOM_SETUP, it just adds its own ROOM_SETUP's content to this array.
@@ -122,7 +122,7 @@ module.exports = {
     LEVEL_CHEAT_CAP: 45,
 
     // Amount of player-bots to spawn.
-    BOTS: 4,
+    BOTS: 0,
 
     // How much XP player-bots get per second until they reach LEVEL_CAP.
     BOT_XP: 1000,
